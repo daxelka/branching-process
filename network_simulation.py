4867005a525f86c75f4d7321ac6a6307018ecf28
Alina <daxelka@yandex.ru>
2022-06-30 11:08:58 +0100

    minor

--- network_simulation.py
+++ network_simulation.py
@@ -5,45 +5,26 @@ from collections import Counter
 import random
 import pandas as pd
 
+# Creating a network
+# network parameters
 p_in = 8/10
 p_out = 1/20
-
 sizes = [5, 5]
 probs = [[p_in, p_out], [p_out, p_in]]
+
 g = nx.stochastic_block_model(sizes, probs, seed=0)
 
 # preparing for drawing
 color_map = ['yellow' if g.nodes[node]['block'] == 0 else 'pink' for node in g.nodes()]
-
 positions = nx.spring_layout(g)
 # nx.draw_networkx(g, positions, node_color=color_map, with_labels=False)
 nx.draw_circular(g, node_color=color_map, with_labels=True)
 plt.show()
 
-
-# Degree Distribution
-def degree_dist(G):
-    # Degrees list
-    degrees = [G.degree(n) for n in G.nodes()]
-
-    # Degree distribution
-    degree_counts = Counter(degrees)
-    keys = sorted(degree_counts.keys())
-
-    values = [degree_counts[k] for k in keys]
-
-    # Dergee distribution normalised
-    # values = [float(i) / sum(values) for i in values]
-    return degrees, values, keys
-
-
-# degrees, values, keys = degree_dist(g)
-# print(degrees, values, keys)
-
-# setting parameters
+# Branching process simulation
+# setting branching process parameters
 prob_infection = 0.5
 n_sim = 2
-# branching process simulation
 # initial seed
 nodes_block0 = [x for x,y in g.nodes(data=True) if y['block']==0]
 seed = random.sample(nodes_block0, 1)
@@ -51,6 +32,7 @@ print(seed)
 active_nodes = seed
 rng = np.random.default_rng()
 results_dic = {'0': [seed]}
+
 for i in range(2):
     results = []
     for active_node in active_nodes:
@@ -61,9 +43,7 @@ for i in range(2):
             infections_np = np.array(infections[0])
             neighbours_np = np.array(neighbours)
             infected_nodes = list(neighbours_np[infections_np > 0])
-            # print('all neighbours', neighbours, 'infections', infections[0])
             if infected_nodes:
-                # print([active_node, infected_nodes])
                 results.append([active_node, infected_nodes])
 
     results_dic[str(i)] = results
