2eacc4164e80c53b7516201248a98a9a94193e39
Daxelka <dubovskaya.a@gmail.com>
2022-07-28 11:23:21 +0100

    modified simulations output and lifetime distribution introduced; issue: nodes deleted after each simulation should be returned back for the next simulation

--- network_simulation.py
+++ network_simulation.py
@@ -12,6 +12,7 @@ sizes = [500, 500]
 probs = [[p_in, p_out], [p_out, p_in]]
 
 g = nx.stochastic_block_model(sizes, probs, seed=0)
+nodes_block0 = [x for x, y in g.nodes(data=True) if y['block'] == 0]
 
 # # preparing for drawing
 # color_map = ['yellow' if g.nodes[node]['block'] == 0 else 'pink' for node in g.nodes()]
@@ -26,17 +27,16 @@ prob_infection = 0.05
 max_generation = 10
 # initial seed from block 0 always
 
+simulations = {}
 
-for i in range(100):
-    nodes_block0 = [x for x, y in g.nodes(data=True) if y['block'] == 0]
+for i in range(5):
     seed = random.sample(nodes_block0, 1)
-    print('seed node:', seed)
     active_nodes = seed
     rng = np.random.default_rng()
-    results_dic = {'0': [seed]}
+    results_dic = {'0': {}}
     generation = 0
     while active_nodes and generation <= max_generation:
-        results = []
+        results = {}
         for active_node in active_nodes:
             # take IDs of all nodes that are connected by a edge which received a possitive outcome in Bernoulli trials
             neighbours = list(g.neighbors(active_node))
@@ -45,13 +45,34 @@ for i in range(100):
                 infections_np = np.array(infections[0])
                 neighbours_np = np.array(neighbours)
                 infected_nodes = list(neighbours_np[infections_np > 0])
-                if infected_nodes:
-                    results.append([active_node, infected_nodes])
+                results['active_nodes'] = active_nodes
+                results['offspring'] = infected_nodes
+                results['n_active'] = len(active_nodes)
+                results['average_n_offspring'] = np.sum(infections)/len(active_nodes)
             # remove current active node
             g.remove_node(active_node)
 
         results_dic[str(generation)] = results
         active_nodes = infected_nodes
         generation += 1
+    # simulations[str(i)] = {'max_generation': generation-1, 'data': results_dic}
+    simulations[str(i)] = results_dic
 
-    print(results_dic)
+print(simulations)
+
+
+def get_max_generation(data):
+    return [len(data[str(j)])-1 for j in range(len(data))]
+
+
+# lifetime distribution
+lifetime_distribution = get_max_generation(simulations)
+print(lifetime_distribution)
+
+plt.hist(lifetime_distribution)
+plt.show()
+
+# tt = [simulations[str(i)]['max_generation'] for i in range(len(simulations))]
+# ttt = [len(simulations[str(i)]['data'])-1 for i in range(len(simulations))]
+# plt.hist(tt)
+# plt.show()
