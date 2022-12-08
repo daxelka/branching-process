605d3c8d6f8194d4e8e4f0c25e9bc31dc7fb1568
Alina <daxelka@yandex.ru>
2022-12-08 19:00:54 +0000

    pdf_calculation_from_pgf corrected

--- pdf_calculation_from_pgf.py
+++ pdf_calculation_from_pgf.py
@@ -2,18 +2,10 @@ import numpy as np
 from scipy.fft import fft, ifft
 import matplotlib.pyplot as plt
 
-x = np.array([1.0, 2.0, 1.0, -1.0, 1.5])
-y = fft(x)
-print(y)
-yinv = ifft(y)
-print(yinv)
-z = np.exp(50.0 * 1.j * 2.0*np.pi*x) + 0.5*np.exp(-80.0 * 1.j * 2.0*np.pi*x)
-print(z)
-
 # that are M point apart
 M = 100
-z_i = np.array(range(M))/M
-x_i = np.exp(2*np.pi*1.j*z_i)
+p_i = np.array(range(M))/M
+x_i = np.exp(-2*np.pi*1.j*p_i)
 
 print(x_i)
 plt.plot(x_i.real, x_i.imag, '.')
@@ -40,7 +32,8 @@ def itt_gen(s1, s2, lambda_param=0.9, n_max=100):
         s1 = get_fx(s1, s2, lambda_param)
         s2 = get_ft(s2)
     # apply initial conditions
-    ans = np.conj(get_initial_condition(s1, s2))
+    # ans = np.conj(get_initial_condition(s1, s2))
+    ans = get_initial_condition(s1, s2)
     return ans
 
 
