eef67cb928de539a7f4514de2273f4c09e27789a
Daxelka <dubovskaya.a@gmail.com>
2022-12-09 16:04:42 +0000

    minor

--- single_run_network.py
+++ single_run_network.py
@@ -18,7 +18,7 @@ community_size = 500  # number of nodes in each communities
 p_in, p_out = lambda_in/community_size, lambda_out/community_size
 
 # number of simulations
-n_sim = 1
+n_sim = 50
 
 bp_network = BranchingProcessNetwork(p_in, p_out, [community_size, community_size], prob_infection)
 
@@ -28,18 +28,45 @@ total_infections = get_total_infections(results_network, community_size, block='
 infections_block0 = get_total_infections(results_network, community_size, block='block0')
 infections_block1 = get_total_infections(results_network, community_size, block='block1')
 
-for sim in range(len(results_network)):
-    # plt.plot(list(range(len(total_infections[str(sim)]))), total_infections[str(sim)])
-    plt.plot(list(range(len(infections_block0[str(sim)]))), infections_block0[str(sim)])
-    plt.plot(list(range(len(infections_block1[str(sim)]))), infections_block1[str(sim)])
-plt.xlabel('generation')
-plt.ylabel('total infections')
-plt.legend(['block 0', 'block 1'], loc='upper right')
-# set axis ticks to integers only
-plt.gca().yaxis.get_major_locator().set_params(integer=True)
-plt.show()
+# for sim in range(len(results_network)):
+#     # plt.plot(list(range(len(total_infections[str(sim)]))), total_infections[str(sim)])
+#     plt.plot(list(range(len(infections_block0[str(sim)]))), infections_block0[str(sim)])
+#     plt.plot(list(range(len(infections_block1[str(sim)]))), infections_block1[str(sim)])
+#     plt.plot(list(range(len(infections_block1[str(sim)]))), total_infections[str(sim)])
+# plt.xlabel('generation')
+# plt.ylabel('total infections')
+# plt.legend(['block 0', 'block 1', 'total'], loc='upper left')
+# # set axis ticks to integers only
+# plt.gca().yaxis.get_major_locator().set_params(integer=True)
+# plt.show()
 
 print('done')
+
+max_generation = 20
+cascades = []
+cascades_size_distribution_np = np.zeros((1,max_generation), dtype='float')
+print(cascades_size_distribution_np)
+# for i in range(len(total_infections)):
+#     cascades = cascades + [list(total_infections[str(i)]) + [0]*(max_generation - len(total_infections[str(i)]))]
+
+for i in range(len(total_infections)):
+    cascades_size_distribution_np = cascades_size_distribution_np + np.array(list(total_infections[str(i)]) + [0]*(max_generation - len(total_infections[str(i)])))
+
+cascades_size_distribution_np = cascades_size_distribution_np/n_sim
+print(cascades_size_distribution_np)
+
+plt.plot(np.array(range(max_generation)), cascades_size_distribution_np[0])
+plt.gca().xaxis.get_major_locator().set_params(integer=True)
+plt.show()
+
+# def get_cascades_distribution_np(total_inf):
+
+# cascade_size_distribution = np.sum(np.array(cascades, dtype='int_'), axis=0) / n_sim
+# print(cascade_size_distribution)
+
+# plt.plot(list(range(max_generation)), cascade_size_distribution)
+# plt.gca().xaxis.get_major_locator().set_params(integer=True)
+# plt.show()
 
 # max_generation = get_max_generation(results_network)
 #
