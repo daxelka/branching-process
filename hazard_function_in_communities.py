bed760c30c537c1dac53b2b2857400e156452c3e
Daxelka <dubovskaya.a@gmail.com>
2023-03-09 16:47:20 +0000

    hazerd function created (2 ways, mine and Davids)

--- hazard_function_in_communities.py
+++ hazard_function_in_communities.py
@@ -5,87 +5,44 @@ import time
 import matplotlib.pyplot as plt
 from bp_mt_class import BranchingProcessMultiType
 from bp_mt_class import get_hazart_function
+from mtbp_analysis_class import MTBPAnalysis
 
 # load Dave's results
-df = pd.read_csv('Davids_code/full_extin_dist_v3.csv')
+df = pd.read_csv('Davids_code/full_extin_dist_v4.csv')
+sim_results = pd.read_csv('data_temp_no_excess_distribution.csv')
 
 # branching process parameters
 seed_1, seed_2 = 1, 0
 lambda_in, lambda_out = 8, 2
 probabilities = list(set(df.pin))
-
+#
 # simulation parameters
-n_simulations = 100
-probability = 0.04
-
+n_simulations = 10000
+probability = 0.09
+#
 probability_in, probability_out = probability, probability
 
-# initiate branching process
-bp = BranchingProcessMultiType(seed_1, seed_2,
-                           lambda_in, lambda_out,
-                           probability_in, probability_out)
+# # initiate branching process
+# bp = BranchingProcessMultiType(seed_1, seed_2,
+#                            lambda_in, lambda_out,
+#                            probability_in, probability_out)
+#
+#
+# # run branching process n_simulation types
+# t_start = time.time()
+# sim_results = bp.run_v2(n_simulations)
+# print('elapsed time:', time.time() - t_start)
+# sim_results.to_csv('data_temp_no_excess_distribution.csv')
 
 
-# run branching process n_simulation types
-t_start = time.time()
-sim_results = bp.run_v2(n_simulations)
-print('elapsed time:', time.time() - t_start)
-
-# sim_results = pd.DataFrame({'generation': [0,1, 0,1,2, 0,1,2,3,],
-#                             'new_infections_1': [1,0, 1,2,0, 1,2,0,0],
-#                             'new_infections_2': [0,0, 0,1,0, 0,0,1,0],
-#                             'simulation_id': [0,0, 1,1,1, 2,2,2,2]})
-
-
-def hazard_function_in_communities(data):
-    max_generation = max(data.generation)
-    hazard_prob_1 = [0]
-    hazard_prob_2 = [0]
-    hazard_prob_both = [0]
-    for gen in range(1, max_generation):
-        data_current_gen = data[data.generation == gen]
-        data_previous_gen = data[data.generation == gen - 1]
-
-        nonzeros_previous_gen_both_communities = np.count_nonzero(
-            data_previous_gen.new_infections_1 + data_previous_gen.new_infections_2)
-        nonzeros_current_gen_both_communities = np.count_nonzero(
-            data_current_gen.new_infections_1 + data_current_gen.new_infections_2)
-        nonzeros_current_gen_1 = np.count_nonzero(data_current_gen.new_infections_1)
-        nonzeros_current_gen_2 = np.count_nonzero(data_current_gen.new_infections_2)
-
-        hazard_prob_1.append(1 - nonzeros_current_gen_1/nonzeros_previous_gen_both_communities)
-        hazard_prob_2.append(1 - nonzeros_current_gen_2 / nonzeros_previous_gen_both_communities)
-        hazard_prob_both.append(1 - nonzeros_current_gen_both_communities / nonzeros_previous_gen_both_communities)
-
-    hazard_function = pd.DataFrame({'gens': list(range(max_generation)),
-                                    'probs_1': hazard_prob_1,
-                                    'probs_2': hazard_prob_2,
-                                    'probs_both': hazard_prob_both})
-    return hazard_function
-
-
-def duration_extinction(data):
-    max_simulation_id = max(data.simulation_id)
-    for simulation in range(max_simulation_id):
-        data_current = data[data.simulation_id == simulation]
-        print(np.count_nonzero(data_current.new_infections_1 == 0))
-
-    return 1
-
-
-# print(sim_results)
-hazard_function = hazard_function_in_communities(sim_results)
+analysis = MTBPAnalysis()
+hazard_function_old = analysis.hazard_function_in_communities(sim_results)
+hazard_function_new = analysis.hazard_function_in_communities_v2(sim_results)
 # duration_extinction(sim_results)
 
 
 
 # visulation
-
-# plt.scatter(hazard_function.gens, hazard_function.probs_both, marker="x")
-# plt.scatter(df.t[df.pin==probability], df.hazard_b[df.pin==probability])
-# plt.scatter(hazard_function.gens, hazard_function.probs_2)
-# plt.scatter(hazard_function.gens, hazard_function.probs_both)
-# plt.show()
 
 # colors
 CB91_Blue = '#2CBDFE'
@@ -98,69 +55,73 @@ CB91_Amber = '#F5B14C'
 color1, color2, color3 = (CB91_Blue, CB91_Green, CB91_Pink)
 my_marker = '.'
 
-plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.sans-serif'] = ['Arial']
+plt.scatter(list(range(len(hazard_function_new))), hazard_function_new.hazard_1, marker=my_marker, c=color1)
+plt.scatter(list(range(len(hazard_function_new))), hazard_function_new.hazard_2, marker=my_marker, c=color2)
+plt.scatter(list(range(len(hazard_function_new))), hazard_function_new.hazard_both, marker=my_marker, c=color3)
 
-fig, axs = plt.subplots(3)
-fig.suptitle("Hazard function, probability of infection: " + str(probability))
-
-fig.set_figheight(6)
-fig.set_figwidth(6)
-
-axs[0].scatter(hazard_function.gens, hazard_function.probs_both, marker=my_marker, c=color1)
-axs[0].plot(df.t[df.pin==probability], df.hazard_b[df.pin==probability], c=color1)
-axs[0].set_title('whole network')
-axs[0].set_ylim([0, 1])
-axs[0].set_xlim([0, 20])
-axs[0].legend(['simulation', 'analytical'], frameon=False, loc='lower right')
-# axs[0].legend(['simulation', 'analytical'], frameon=False, loc='upper left')
-
-axs[1].scatter(hazard_function.gens, hazard_function.probs_1, marker=my_marker, c=color2)
-axs[1].plot(df.t[df.pin==probability], df.hazard_1[df.pin==probability], c=color2)
-axs[1].set_title('community #1')
-axs[1].set_ylim([0, 1])
-axs[1].set_xlim([0, 20])
-axs[1].legend(['simulation', 'analytical'], frameon=False, loc='lower right')
-# axs[1].legend(['simulation', 'analytical'], frameon=False, loc='upper left')
-
-axs[2].scatter(hazard_function.gens, hazard_function.probs_2, marker=my_marker, c=color3)
-axs[2].plot(df.t[df.pin==probability], df.hazard_2[df.pin==probability], c=color3)
-axs[2].set_title('community #2')
-axs[2].set_ylim([0, 1])
-axs[2].set_xlim([0, 20])
-axs[2].legend(['simulation', 'analytical'], frameon=False, loc='lower right')
-# axs[2].legend(['simulation', 'analytical'], frameon=False, loc='lower left')
-
-axs[2].xaxis.get_major_locator().set_params(integer=True)
-
-for ax in axs.flat:
-    ax.set(xlabel='generation', ylabel='probability')
-
-# Hide x labels and tick labels for top plots and y ticks for right plots.
-for ax in axs.flat:
-    ax.label_outer()
-
-# fig.savefig('img/hazard_function_p_'+ str(probability)+'.png', dpi=300)
+plt.plot(df.t[df.pin==probability], df.hazard_1[df.pin==probability], c=color1)
+plt.plot(df.t[df.pin==probability], df.hazard_2[df.pin==probability], c=color2)
+plt.plot(df.t[df.pin==probability], df.hazard_b[df.pin==probability], c=color3)
+plt.title("Hazard function, probability of infection: " + str(probability))
+plt.ylim([0, 1])
+plt.xlim([0, 20])
+plt.xlabel('generation')
+plt.ylabel('hazard function')
+plt.legend(['type 1 simulation', 'type 2 simulation', 'both types simulation',
+            'type 1 analytical', 'type 2 analytical', 'both types analytical'], frameon=False, loc='upper right')
+plt.gca().xaxis.get_major_locator().set_params(integer=True)
 plt.show()
 
+# plt.scatter(hazard_function.gens, hazard_function.probs_both, marker="x")
+# plt.scatter(df.t[df.pin==probability], df.hazard_b[df.pin==probability])
+# plt.scatter(hazard_function.gens, hazard_function.probs_2)
+# plt.scatter(hazard_function.gens, hazard_function.probs_both)
+# plt.show()
 
-# fig = plt.figure(figsize=(8, 6))
+# # colors
+# CB91_Blue = '#2CBDFE'
+# CB91_Green = '#47DBCD'
+# CB91_Pink = '#F3A0F2'
+# CB91_Purple = '#9D2EC5'
+# CB91_Violet = '#661D98'
+# CB91_Amber = '#F5B14C'
+#
+# color1, color2, color3 = (CB91_Blue, CB91_Green, CB91_Pink)
+# my_marker = '.'
+#
+# plt.rcParams['font.family'] = 'sans-serif'
+# plt.rcParams['font.sans-serif'] = ['Arial']
+#
+# fig, axs = plt.subplots(3)
 # fig.suptitle("Hazard function, probability of infection: " + str(probability))
 #
-# axs[0].scatter(hazard_function.gens, hazard_function.probs_both, marker="x", c=color1)
+# fig.set_figheight(6)
+# fig.set_figwidth(6)
+#
+# axs[0].scatter(hazard_function.gens, hazard_function.probs_both, marker=my_marker, c=color1)
 # axs[0].plot(df.t[df.pin==probability], df.hazard_b[df.pin==probability], c=color1)
 # axs[0].set_title('whole network')
-# axs[0].legend(['analytical', 'simulation'], frameon=False, loc='lower right')
+# axs[0].set_ylim([0, 1])
+# axs[0].set_xlim([0, 20])
+# axs[0].legend(['simulation', 'analytical'], frameon=False, loc='lower right')
+# # axs[0].legend(['simulation', 'analytical'], frameon=False, loc='upper left')
 #
-# axs[1].scatter(hazard_function.gens, hazard_function.probs_1, marker="x", c=color2)
+# axs[1].scatter(hazard_function.gens, hazard_function.probs_1, marker=my_marker, c=color2)
 # axs[1].plot(df.t[df.pin==probability], df.hazard_1[df.pin==probability], c=color2)
 # axs[1].set_title('community #1')
-# axs[1].legend(['analytical', 'simulation'], frameon=False, loc='lower right')
+# axs[1].set_ylim([0, 1])
+# axs[1].set_xlim([0, 20])
+# axs[1].legend(['simulation', 'analytical'], frameon=False, loc='lower right')
+# # axs[1].legend(['simulation', 'analytical'], frameon=False, loc='upper left')
 #
-# axs[2].scatter(hazard_function.gens, hazard_function.probs_2, marker="x", c=color3)
+# axs[2].scatter(hazard_function.gens, hazard_function.probs_2, marker=my_marker, c=color3)
 # axs[2].plot(df.t[df.pin==probability], df.hazard_2[df.pin==probability], c=color3)
 # axs[2].set_title('community #2')
-# axs[2].legend(['analytical', 'simulation'], frameon=False, loc='lower right')
+# axs[2].set_ylim([0, 1])
+# axs[2].set_xlim([0, 20])
+# axs[2].legend(['simulation', 'analytical'], frameon=False, loc='lower right')
+# # axs[2].legend(['simulation', 'analytical'], frameon=False, loc='lower left')
+#
 # axs[2].xaxis.get_major_locator().set_params(integer=True)
 #
 # for ax in axs.flat:
@@ -170,5 +131,6 @@ plt.show()
 # for ax in axs.flat:
 #     ax.label_outer()
 #
-# fig.savefig('img/hazard_function_p_'+ str(probability)+'.png', dpi=300)
+# # fig.savefig('img/hazard_function_p_'+ str(probability)+'.png', dpi=300)
 # plt.show()
+
--- mtbp_analysis_class.py
+++ mtbp_analysis_class.py
@@ -4,21 +4,21 @@ import pandas as pd
 
 class MTBPAnalysis:
 
-    def hazard_function_in_communities(results_df):
+    def hazard_function_in_communities(self,results_df):
         max_generation = max(results_df.generation)
         hazard_prob_1 = [0]
         hazard_prob_2 = [0]
         hazard_prob_both = [0]
         for gen in range(1, max_generation):
-            data_current_gen = results_df[results_df.generation == gen]
-            data_previous_gen = results_df[results_df.generation == gen - 1]
+            current_gen = results_df[results_df.generation == gen]
+            previous_gen = results_df[results_df.generation == gen - 1]
 
             nonzeros_previous_gen_both_communities = np.count_nonzero(
-                data_previous_gen.new_infections_1 + data_previous_gen.new_infections_2)
+                previous_gen.new_infections_1 + previous_gen.new_infections_2)
             nonzeros_current_gen_both_communities = np.count_nonzero(
-                data_current_gen.new_infections_1 + data_current_gen.new_infections_2)
-            nonzeros_current_gen_1 = np.count_nonzero(data_current_gen.new_infections_1)
-            nonzeros_current_gen_2 = np.count_nonzero(data_current_gen.new_infections_2)
+                current_gen.new_infections_1 + current_gen.new_infections_2)
+            nonzeros_current_gen_1 = np.count_nonzero(current_gen.new_infections_1)
+            nonzeros_current_gen_2 = np.count_nonzero(current_gen.new_infections_2)
 
             hazard_prob_1.append(1 - nonzeros_current_gen_1 / nonzeros_previous_gen_both_communities)
             hazard_prob_2.append(1 - nonzeros_current_gen_2 / nonzeros_previous_gen_both_communities)
@@ -28,6 +28,29 @@ class MTBPAnalysis:
                                         'probs_1': hazard_prob_1,
                                         'probs_2': hazard_prob_2,
                                         'probs_both': hazard_prob_both})
+        return hazard_function
+
+
+    def hazard_function_in_communities_v2(self, results_df):
+        results_df['new_infections_both'] = results_df['new_infections_1'] + results_df['new_infections_2']
+        hazard_function = (results_df
+                          .assign(new_infections_1_prev_gen=lambda x: x['new_infections_1'].shift(1),
+                                  new_infections_2_prev_gen=lambda x: x['new_infections_2'].shift(1),
+                                  new_infections_both_prev_gen=lambda x: x['new_infections_both'].shift(1))
+                          .query('new_infections_both_prev_gen > 0')
+                          .assign(new_infections_1_true=lambda x: (x['new_infections_1'] == 0).replace({True: 1, False: 0}),
+                                  new_infections_2_true=lambda x: (x['new_infections_2'] == 0).replace({True: 1, False: 0}),
+                                  new_infections_both_true=lambda x: (x['new_infections_both'] == 0).replace({True: 1, False: 0}))
+                          .groupby('generation')
+                          .agg(hazard_1=('new_infections_1_true', 'mean'),
+                               hazard_2=('new_infections_2_true', 'mean'),
+                               hazard_both=('new_infections_both_true', 'mean'))
+                          )
+
+        # adding a row on top for the o generation
+        row_generation_0 = pd.DataFrame({'hazard_1': 0, 'hazard_2': 0, 'hazard_3': 0}, index=[0])
+        hazard_function = pd.concat([row_generation_0, hazard_function]).reset_index(drop=True)
+
         return hazard_function
 
     def get_max_simulation_id(self, results_df):
@@ -55,13 +78,16 @@ class MTBPAnalysis:
 
     def duration_extinction(self, results_df):
         max_simulation_id = max(results_df.simulation_id)
+        extinction_arr = []
         for simulation in range(max_simulation_id + 1):
             data_current = results_df[results_df.simulation_id == simulation]
             # print(np.count_nonzero(data_current.new_infections_1 == 0))
             print('data', np.array(data_current.new_infections_1))
             print('extint',self.zero_sequences(np.array(data_current.new_infections_1)))
 
-        return 1
+            extinction_arr = extinction_arr + self.zero_sequences(np.array(data_current.new_infections_1))
+
+        return extinction_arr
 
 
     def zero_sequences(self, arr):
